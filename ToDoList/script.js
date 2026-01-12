eebb982d424c618ded486034d1e68a121d0aefd4
DIlluck <fadhilsaid50@gmail.com>
2026-01-13 00:29:51 +0700

    finalresult

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,23 +1,29 @@
 const taskInput = document.getElementById('newTaskInput');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
+
+function addClickListener(item) {
+    item.addEventListener('click', function() {
+        item.classList.toggle('toDoCompleted');
+    });
+}
+
 addTaskButton.addEventListener('click', function() {
     const taskInputValue = taskInput.value;
     console.log(taskInputValue);
     const listItem = document.createElement('li');
     const textListItem = document.createTextNode(taskInputValue);
-    const appendTaskList = taskList.appendChild(listItem);
     const emptyText = taskInputValue === "";
     if (!emptyText) {
         listItem.appendChild(textListItem);
+        listItem.classList.add('toDoItem'); 
+        taskList.appendChild(listItem);
+        addClickListener(listItem); 
         taskInput.value = "";
     } else {
         alert("Masukkan Tugas Terlebih Dahulu!");
     }
-
-listItem.addEventListener('click', function() {
-        listItem.style.textDecoration = "line-through";
-        listItem.style.cursor = "pointer";
-        listItem.style.color = "gray";
-    });
 });
+
+const toDoItems = document.querySelectorAll('.toDoItem');
+toDoItems.forEach(addClickListener);
